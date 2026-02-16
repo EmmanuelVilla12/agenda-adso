792f5d7d33b768eade7c9c3ca20332fcf42bcf08
Emmanuel Villa“ <emmanuelvv2008@gmail.com>
2026-02-16 00:35:34 -0500

    Versión inicial - Componente Hola SENA

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+export default function App() { const fecha = new Date().toLocaleString(); return ( <main> <h1>Hola soy emmanuel</h1><h2>Estudiando ADSO </h2> <p>Como aprendiz de ADSO, quiero aprender desarrollo web para crear páginas y aplicaciones dinámicas, funcionales y seguras. Me interesa mejorar mis habilidades en frontend y backend, trabajar con bases de datos y aplicar buenas prácticas para desarrollarme profesionalmente en el área tecnológica.</p></main>)}
